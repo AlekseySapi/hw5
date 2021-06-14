6fbed208c9ba0423bbce69d311428fd465a7ab41
Aleksey L <52339899+AlekseySapi@users.noreply.github.com>
2021-06-14 09:35:12 +0300

    5.3, 5.4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,42 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
+
+    private static int[] bigArr;
+    private static int[] copyBigArr;
+    private static long startTime;
+    private static int n;
 
     public static void main(String[] args) {
 
-        int n = 100;
+//        Задание 5.2
+        n = 100;
 //        countdownInf(n);
         countdown(n);
+
+
+/*
+        Задание 5.3
+        Пример стека вызова: стопка тарелок - полодить тарелку можно только наверх и взять только сверху.
+        Пример стека вызова с рекурсией: факториал, поиск.
+ */
+
+//        Задание 5.4
+        n = 100;
+        startTime = System.nanoTime();
+        while (n > 0) {
+            System.out.println(n);
+            n--;
+        }
+        System.out.println("Печать чисел через цикл прошла за " + (System.nanoTime() - startTime) + " нс");
+
+        System.out.println();
+
+        n = 100;
+        startTime = System.nanoTime();
+        countdown(n);
+        System.out.println("Печать чисел через рекурсию прошла за " + (System.nanoTime() - startTime) + " нс");
 
     }
 
@@ -20,5 +52,8 @@ public class Main {
         }
         return countdown(n - 1);
     }
+
+
+
 
 }
